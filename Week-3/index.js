b3fb2d1e97ac680b141bfa5081ef7f269cf96b3d
Shivama <shivamasthakur.02@gmail.com>
2024-02-05 03:07:24 +0530

    week-3 completed

--- Week-3/index.js
+++ Week-3/index.js
@@ -2,169 +2,169 @@ const express = require("express")
 const app = express()
 
 // An ugly way to add Authentication and Input Validation
-// app.get("/healthy",(req,res)=>{
-//     const kidneyId = req.query.kidneyId
-//     const uname = req.headers.unsame
-//     const pass = req.headers.pass 
+app.get("/healthy",(req,res)=>{
+    const kidneyId = req.query.kidneyId
+    const uname = req.headers.unsame
+    const pass = req.headers.pass 
 
-//     if(uname != "shivama" || pass != "pass"){
-//         res.status(403).json({
-//             msg: "This user doesn't exist!!"
-//         })
-//         return;
-//     }
-//     if(kidneyId != 1 && kidneyId != 2){
-//         res.status(411).json({
-//             msg: "Wrong Inputs"
-//         })
-//         return;
-//     }
-//     res.send("Healthy")
-// })
+    if(uname != "shivama" || pass != "pass"){
+        res.status(403).json({
+            msg: "This user doesn't exist!!"
+        })
+        return;
+    }
+    if(kidneyId != 1 && kidneyId != 2){
+        res.status(411).json({
+            msg: "Wrong Inputs"
+        })
+        return;
+    }
+    res.send("Healthy")
+})
 
-// app.listen(3000)
+app.listen(3000)
 
 // Now, lets say we have to add other route for kidney replacement using put request
 //We will have to do checks 2 times
 //Like this
 
-// app.get("/healthy",(req,res)=>{
-//     const kidneyId = req.query.kidneyId
-//     const uname = req.headers.unsame
-//     const pass = req.headers.pass 
+app.get("/healthy",(req,res)=>{
+    const kidneyId = req.query.kidneyId
+    const uname = req.headers.unsame
+    const pass = req.headers.pass 
 
-//     if(uname != "shivama" && pass != "pass"){
-//         res.status(403).json({
-//             msg: "This user doesn't exist!!"
-//         })
-//         return;
-//     }
-//     if(kidneyId != 1 && kidneyId != 2){
-//         res.status(411).json({
-//             msg: "Wrong Inputs"
-//         })
-//         return;
-//     }
-//     //Fulfil Get Request
-//     res.send("Healthy")
-// })
+    if(uname != "shivama" && pass != "pass"){
+        res.status(403).json({
+            msg: "This user doesn't exist!!"
+        })
+        return;
+    }
+    if(kidneyId != 1 && kidneyId != 2){
+        res.status(411).json({
+            msg: "Wrong Inputs"
+        })
+        return;
+    }
+    //Fulfil Get Request
+    res.send("Healthy")
+})
 
-// app.put("/healthy",(req,res)=>{
-//     const kidneyId = req.query.kidneyId
-//     const uname = req.headers.unsame
-//     const pass = req.headers.pass 
+app.put("/healthy",(req,res)=>{
+    const kidneyId = req.query.kidneyId
+    const uname = req.headers.unsame
+    const pass = req.headers.pass 
 
-//     if(uname != "shivama" || pass != "pass"){
-//         res.status(403).json({
-//             msg: "This user doesn't exist!!"
-//         })
-//         return;
-//     }
-//     if(kidneyId != 1 && kidneyId != 2){
-//         res.status(411).json({
-//             msg: "Wrong Inputs"
-//         })
-//         return;
-//     }
+    if(uname != "shivama" || pass != "pass"){
+        res.status(403).json({
+            msg: "This user doesn't exist!!"
+        })
+        return;
+    }
+    if(kidneyId != 1 && kidneyId != 2){
+        res.status(411).json({
+            msg: "Wrong Inputs"
+        })
+        return;
+    }
 
-//     //Fulfil Put request
+    //Fulfil Put request
 
-//     res.send("Healthy")
-// })
+    res.send("Healthy")
+})
 
-// app.listen(3000)
+app.listen(3000)
 
 //This will violate DRY principle i.e. Don't Repeat Yourself
 //To tackle this, we can use a wrapper function to look out for validation and authentication
 //Like this...
 
 
-// function authetication(uname,pass){
-//     if(uname != "shivama" && pass != "pass"){
-//         return false;
-//     }
-//     return true;
-// }
-// function inputValidation(kidneyId){
-//     if(kidneyId != 1 && kidneyId != 2){
-//         return false;
-//     }
-//     return true;
-// }
+function authetication(uname,pass){
+    if(uname != "shivama" && pass != "pass"){
+        return false;
+    }
+    return true;
+}
+function inputValidation(kidneyId){
+    if(kidneyId != 1 && kidneyId != 2){
+        return false;
+    }
+    return true;
+}
 
-// app.get('/healthy',(req,res)=>{
-//     const kidneyId = req.query.kidneyId
-//     if(!authetication(req.query.uname,req.query.pass)){
-//         res.status(403).json({
-//             msg: "User doesn't exist..."
-//         })
-//         return
-//     }
-//     if(!inputValidation(kidneyId)){
-//         res.status(411).json({
-//             msg : "wrong input..."
-//         })
-//     }
-//     //logic for get request
-//     res.send("Healthy")
-// })
-// app.put('/healthy',(req,res)=>{
-//     const kidneyId = req.query.kidneyId
-//     if(!authetication(req.query.uname,req.query.pass)){
-//         res.status(403).json({
-//             msg: "User doesn't exist..."
-//         })
-//         return
-//     }
-//     if(!inputValidation(kidneyId)){
-//         res.status(411).json({
-//             msg : "wrong input..."
-//         })
-//     }
-//     //logic for PUT request
-//     res.send("Healthy")
-// })
+app.get('/healthy',(req,res)=>{
+    const kidneyId = req.query.kidneyId
+    if(!authetication(req.query.uname,req.query.pass)){
+        res.status(403).json({
+            msg: "User doesn't exist..."
+        })
+        return
+    }
+    if(!inputValidation(kidneyId)){
+        res.status(411).json({
+            msg : "wrong input..."
+        })
+    }
+    //logic for get request
+    res.send("Healthy")
+})
+app.put('/healthy',(req,res)=>{
+    const kidneyId = req.query.kidneyId
+    if(!authetication(req.query.uname,req.query.pass)){
+        res.status(403).json({
+            msg: "User doesn't exist..."
+        })
+        return
+    }
+    if(!inputValidation(kidneyId)){
+        res.status(411).json({
+            msg : "wrong input..."
+        })
+    }
+    //logic for PUT request
+    res.send("Healthy")
+})
 
-// app.listen(3000)
+app.listen(3000)
 
 //But, this is also a cluttered way of reusing stuff as we can see we have to deal with big code
 //let's see another way to do it
 
-// function authentication(req,res,next){
-//     const uname = req.headers.uname
-//     const pass = req.headers.pass 
-//     if(uname != "shivama" && pass != "pass"){
-//         res.status(403).json({
-//             msg : "User doesn't exist"
-//         })
-//     }
-//     else{
-//         next() //else passed on to next middleware
-//     }
-// }
-// function validation(req,res,next){
-//     const kidneyId = req.query.kidneyId 
-//     if(kidneyId != 1 && kidneyId != 2){
-//         res.status(411).json({
-//             msg : "input values wrong"
-//         })
-//     }
-//     else{
-//         next() // else passed on to next middleware
-//     }
-// }
+function authentication(req,res,next){
+    const uname = req.headers.uname
+    const pass = req.headers.pass 
+    if(uname != "shivama" && pass != "pass"){
+        res.status(403).json({
+            msg : "User doesn't exist"
+        })
+    }
+    else{
+        next() //else passed on to next middleware
+    }
+}
+function validation(req,res,next){
+    const kidneyId = req.query.kidneyId 
+    if(kidneyId != 1 && kidneyId != 2){
+        res.status(411).json({
+            msg : "input values wrong"
+        })
+    }
+    else{
+        next() // else passed on to next middleware
+    }
+}
 
-// app.get('/healthy',authentication,validation,(req,res)=>{
-//     //logic to get
-//     res.send("healthy")
-// })
+app.get('/healthy',authentication,validation,(req,res)=>{
+    //logic to get
+    res.send("healthy")
+})
 
-// app.put('/healthy',authentication,validation,(req,res)=>{
-//     //logic to put
-//     res.send("healthy")
-// })
-// //and so on...
-// app.listen(3000)
+app.put('/healthy',authentication,validation,(req,res)=>{
+    //logic to put
+    res.send("healthy")
+})
+//and so on...
+app.listen(3000)
 
 
 //app.use()
@@ -174,36 +174,36 @@ const app = express()
 
 //Why Input Validation is Required?
 
-// app.use(express.json())
+app.use(express.json())
 
-// app.post('/healthy',(req,res)=>{
-//     const kidneys = req.body.kidneys 
-//     const kidneyLength = kidneys.length 
+app.post('/healthy',(req,res)=>{
+    const kidneys = req.body.kidneys 
+    const kidneyLength = kidneys.length 
 
-//     res.send("you have "+kidneyLength+" kidneys.")
+    res.send("you have "+kidneyLength+" kidneys.")
 
-// })
-// app.listen(3000)
+})
+app.listen(3000)
 
 //Here, if the client sends a post request with kidneys as a number , or nothing, or anything gibberish , we should be able to handle these. That is the reason we need 
 //input validation
 
-// app.use(express.json())
+app.use(express.json())
 
-// app.post('/healthy',(req,res)=>{
-//     const kidneys = req.body.kidneys 
+app.post('/healthy',(req,res)=>{
+    const kidneys = req.body.kidneys 
 
-//     if(!kidneys){
-//         res.json({
-//             msg : "Empty Input"
-//         })
-//     }
-//     else{
-//         const kidneyLength = kidneys.length 
-//         res.send("you have "+kidneyLength+" kidneys.")
-//     }
-// })
-// app.listen(3000)
+    if(!kidneys){
+        res.json({
+            msg : "Empty Input"
+        })
+    }
+    else{
+        const kidneyLength = kidneys.length 
+        res.send("you have "+kidneyLength+" kidneys.")
+    }
+})
+app.listen(3000)
 
 //This is one way to validate inputs. But, for multiple validation checks , we will have multiple if else statements which is ugly.
 //That's why, we use Zod Library
@@ -214,34 +214,34 @@ const app = express()
 //For this , we use global catches
 //It is used at the end of code
 
-// app.use((err,req,res,next)=>{
-//     res.json({
-//         msg : "Something went wrong ... "
-//     })
-// })
+app.use((err,req,res,next)=>{
+    res.json({
+        msg : "Something went wrong ... "
+    })
+})
 
 //ZOD - AN INPUT VALIDATION LIBRARY
 
-// const zod = require("zod")
-// app.use(express.json())
+const zod = require("zod")
+app.use(express.json())
 
-// const schema = zod.array(zod.number()) //make a schema which defines the structure of input. Here , it is array of numbers
+const schema = zod.array(zod.number()) //make a schema which defines the structure of input. Here , it is array of numbers
 
-// app.post('/healthy',(req,res)=>{
-//     const kidneys = req.body.kidneys 
-//     const response = schema.safeParse(kidneys)
-//     if(!response.success){
-//         res.status(411).json({
-//             msg : "Invalid Inputs"
-//         })
-//     }
-//     else{
-//         res.send({
-//             response
-//         })
-//     }
-// })
-// app.listen(3000)
+app.post('/healthy',(req,res)=>{
+    const kidneys = req.body.kidneys 
+    const response = schema.safeParse(kidneys)
+    if(!response.success){
+        res.status(411).json({
+            msg : "Invalid Inputs"
+        })
+    }
+    else{
+        res.send({
+            response
+        })
+    }
+})
+app.listen(3000)
 
 //let's say we want to have a schema like this...
 //  {
--- Week-3/lecture3_2.js
+++ Week-3/lecture3_2.js
@@ -6,15 +6,98 @@
 
 const express = require("express")
 const app = express()
+const jwt = require("jsonwebtoken")
+const jwtpass = "123456"
+const mongoose = require("mongoose")
+mongoose.connect("mongodb+srv://admin:FRYJKDCFErW5sbZd@cluster0.yqyjkwo.mongodb.net/user_app")
+
 app.use(express.json())
+
+//In-memory-data
+const USERS = [
+    {
+        username : "shivama",
+        password : "123",
+        name : "shiva"
+    },
+    {
+        username : "rahul",
+        password : "1234",
+        name : "raj"
+    },
+    {
+        username : "ram",
+        password : "12345",
+        name : "rama"
+    }
+]
+
+const User = mongoose.model("User",{
+    name : String,
+    username : String,
+    password : String,
+})
+
+//to be modified as per mongodb
+async function userExists(name,username,password){
+    let res = false
+    //For IN_MEMORY_DATA
+    for(let i=0;i<USERS.length;i++){
+        if(USERS[i].username === username && USERS[i].password === password){
+            return true
+        }
+    }
+    //same, but using  find()
+    const foundobj = USERS.find(obj => obj.username === username && obj.password === password)
+    if(foundobj){
+        res = true
+    }
+    return res
+}
+
+
+// Signup Functionality
+app.post("/signup",async function(req,res){
+
+    const name = req.body.name 
+    const username = req.body.username 
+    const password = req.body.password 
+
+
+    const existingUser = await User.findOne({username: username});
+    if(existingUser){
+        return res.status(400).json({
+            msg : "Username already exists"
+        })
+    }
+    const user = new User({
+        name : name,
+        username : username,
+        password : password
+    })
+    user.save()
+    res.json({
+        msg : "User created successfully..."
+    })
+})
 
 //First, a POST request at /signup, which will take username and password as input from the body of the request, encrypt username which will be sent as JWT
 
-app.post("/signup",(req,res)=>{
+app.post("/signin",(req,res)=>{
 
     const username = req.body.username 
     const password = req.body.password 
 
+    if(!userExists(username,password)){
+        return res.status(403).json({
+            msg : "User doesn't exist in our DB"
+        })
+    }
+
+    var token = jwt.sign({username : username},jwtpass)
+    return res.json({
+        token
+    })
 
 })
 
@@ -22,5 +105,26 @@ app.post("/signup",(req,res)=>{
 
 app.get("/users",(req,res)=>{
 
-
+    const token = req.headers.authorization
+    try{
+        const decoded = jwt.verify(token,jwtpass)
+        const username = decoded.username 
+        res.json({   
+            users : USERS.filter(function(val){                 //returning a list of users except the username which was tokenized
+                if(val.username == username){
+                    return false
+                }
+                else{
+                    return true
+                }
+            })
+        })
+    }catch(err){
+        return res.status(403).json({
+            msg : "Invalid token"
+        })
+    }
 })
+
+app.listen(3000)
+
