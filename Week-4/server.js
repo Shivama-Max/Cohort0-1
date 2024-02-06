fdd4174cf9100b6e40cdc2e1ab5d12b86cf44d6e
Shivama <shivamasthakur.02@gmail.com>
2024-02-06 19:34:50 +0530

    week 4

--- Week-4/server.js
+++ Week-4/server.js
@@ -1,5 +1,8 @@
 const express = require("express")
+const cors = require("cors")
 const app = express()
+
+app.use(cors())
 
 app.get("/sum",(req,res)=>{
     let num1 = req.query.a
@@ -7,6 +10,19 @@ app.get("/sum",(req,res)=>{
     let result = parseInt(num1)+parseInt(num2)
 
     res.send(`${result}`) //can use result.toString() as well 
+})
+
+app.get("/interest",(req,res)=>{
+    let principal = req.query.principal
+    let rate = req.query.rate
+    let time = req.query.time
+
+    let interest = (parseInt(principal)*parseInt(rate)*parseInt(time))/100;
+    let total = parseInt(principal) + interest
+    res.json({
+        total : total,
+        interest : interest,
+    })
 })
 
 app.listen(3000)
